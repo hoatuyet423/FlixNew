ddd19d1ad3d586471fa49c9a190dfe4f0c1cc48d
Jenny Quach <65835856+hoatuyet423@users.noreply.github.com>
2022-02-03 01:50:04 -0800

    create a flix app with tableview show poster, title and description of the movie

---
+++ FlixNew/MovieViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieViewCell.swift
+//  FlixNew
+//
+//  Created by Jenny Quach on 2/2/22.
+//
+
+import UIKit
+
+class MovieViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
